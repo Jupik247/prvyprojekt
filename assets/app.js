eb34120d22419ff9c9499e3fb01cc64ce1cf3592
Dominik <pumpsomeemail>
2023-11-08 13:15:56 +0100

    Edit 1

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+let prvaPremenna = 3;
+let druhaPremenna = 7;
+
+let vysledok01 = prvaPremenna + druhaPremenna;
+
+//alert("3 + 7");
+//alert(3 + 7);
+//alert("prvaPremenna + druhaPremenna");
+//alert(prvaPremenna + druhaPremenna);
+//alert("vysledok01");
+//alert(vysledok01);
+
+let moj vek= 17
